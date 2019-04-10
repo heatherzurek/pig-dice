869ead956fd158c223a6093b068fb4bc63053496
Heather Zurek and Liz Kelley <heather@>
2019-04-10 16:30:05 -0700

    add turn swap, and check for rolling a 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,62 @@
-function Player(id) {
-  this.playerId = id,
-  this.diceRoll = 0,
-  this.turnScore = 0,
-  this.totalScore = 0,
-  this.active = false
+function Player() {
+  this.diceRoll = 0,  //last rolled die
+  this.turnScore = 0, //current score for turn
+  this.totalScore = 0 //total score for all turns
 }
 
-function game() {
-  this.currentPlayer = "playerOne"
-}
-
-function pressHold() {
-  $('#hold').on('click', function() {
-    playerOne.totalScore += playerOne.turnScore;
-  });
-}
-//Player1, Player2
-//click roll to roll #
-// take # put it below the active player
-// adds numbers below together
-// click hold to move total into scorecard
-// switch player ability
-
-// Player.prototype.score = function(score) {
-//   this.score += score;
-// }
-
-//to create our two players
-var playerOne = new Player(1);
-var playerTwo = new Player(2);
-playerOne.active = true;
-
-Player.prototype.diceRoll = function() {
+Player.prototype.rollDice = function() {
   var roll = Math.floor(Math.random()*6) + 1;
-  this.turnScore += roll;
-  // check active player x
-  // if(playerOne.active === true) {
-  //   if(roll === 1) {
-  //     playerOne.turnScore = 0;
-  //     playerOne.active = false;
-  //     playerTwo.active = true;
-  //   } else if(roll !== 1) {
-  //     playerOne.turnScore += roll;
-  //   } else {
-  //
-  //   }
-  //   //do all this to p1 x
-  //   //if roll === 1: turnScore = 0, active = false & other player active = true  x
-  //   // put roll in playerOne.diceRoll/turnscore  x
-  //   // add that number to playerOne.turnScore
-  // } else if (playerTwo.active === true) {
-  //     if (roll === 1) {
-  //       playerTwo.turnScore = 0;
-  //       playerTwo.active = false;
-  //       playerOne.active = true;
-  //     }
-    // do all this to p2 x
-    //if roll === 1: turnScore = 0, active = false & other player active = true x
-    // put roll in playerTwo.diceRoll
-    // add that number to playerTwo.turnScore
-  // }
+  this.diceRoll = roll;
+  if (roll === 1) {
+    this.turnScore = 0;
+    myGame.playerTwoTurn = !myGame.playerTwoTurn
+  } else {
 
+    this.turnScore += roll;
+  }
 }
 
-//CLICK HOLD
-// take turnScore and add to totalScore
-// if totalScore > 100 YOU WIN!
-// else
-// switch status to inactive
-// switch other player to active
 
 
-//   if (roll == 1 ) {
-//   this.diceRoll == 1;
-//   this.turnScore == 0;
-// } else {
-//   this.discRoll == roll;
-//   this.turnScore += roll;
-// };
+function Game() {
+  this.playerTwoTurn = false;
+  this.players = [];
+}
+
+Game.prototype.getCurrentPlayer = function () {
+  if (this.playerTwoTurn === true ) {
+  return this.players[1]
+} else if (this.playerTwoTurn === false ) {
+  return this.players[0]
+  }
+}
+
+// function pressHold() {
+//   $('#hold').on('click', function() {
+//     playerOne.totalScore += playerOne.turnScore;
+//   });
 // }
 
-function attachRollListeners() {
-  $("#roll").on("click", function() {
-    var result = diceRoll();
-    $("#results").text(result);
-  });
-}
+var myGame;
 
 //front end business
+function attachRollListeners() {
+  $("#roll").on("click", function() {
+    var player = myGame.getCurrentPlayer();
+    player.rollDice();
+    $("#rollResults").text(player.diceRoll);
+    $("#totalResults").text(player.turnScore);
+    console.log(myGame.playerTwoTurn);
+  });
+}
+
 $(document).ready(function() {
+  myGame = new Game();
+  player1 = new Player();
+  player2 = new Player();
+
+  myGame.players.push(player1, player2);
+
   attachRollListeners();
   $("#piggieDice").submit(function(event) {
     event.preventDefault();
